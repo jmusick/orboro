1e773a24af8ed4720dd72b04e40d219f6b13a47a
Justin Musick <jd@orboro.net>
2026-09-18 12:48:22 -0400

    Release: v1.32.0 - add creator accuracy article header

--- scripts/content/which-wow-creators-predicted-midnight-season-2-mythic-plus-meta-best.sql
+++ scripts/content/which-wow-creators-predicted-midnight-season-2-mythic-plus-meta-best.sql
@@ -113,7 +113,7 @@ For the complete written record, use the [shared prediction review workbook](htt
   CAST(strftime('%s', 'now') AS INTEGER) * 1000,
   CAST(strftime('%s', 'now') AS INTEGER) * 1000,
   CAST(strftime('%s', 'now') AS INTEGER) * 1000,
-  NULL
+  '/images/which-wow-creators-predicted-midnight-season-2-mythic-plus-meta-best-header.webp'
 )
 ON CONFLICT(slug) DO UPDATE SET
   title = excluded.title,
